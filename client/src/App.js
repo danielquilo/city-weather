aef08635af07d194e10064fc1221ae59b2859758
danielquilo <danielquilo1012@yahoo.com>
2023-05-12 17:15:49 +0100

    test

---
+++ client/src/App.js
@@ -0,0 +1,74 @@
+import './App.css';
+import axios from "axios";
+import { useState } from "react";
+
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [location, setLocation] = useState({});
+  const [map, setMap] = useState("");
+  const [apiError, setApiError] = useState("");
+  const [weather, setWeather] = useState([]);
+  const [darkTheme, SetDarkTheme] = useState (true);
+
+
+function handleChange(event) {
+  setSearchQuery(event.target.value);
+}
+
+async function getLocation() {
+  try {
+    // https://eu1.locationiq.com/v1/search?key=YOUR_ACCESS_TOKEN&q=SEARCH_STRING&format=json
+    const API = `https://eu1.locationiq.com/v1/search?key=${process.env.REACT_APP_API_KEY}&q=${searchQuery}&format=json`;
+    const res = await axios.get(API);
+    const newLocation = res.data[0];
+    setLocation(newLocation);
+    getMap(newLocation);
+    getWeather(newLocation);
+    setApiError("");
+  } catch (error) {
+    console.log(error);
+    setApiError(error.message);
+    setLocation({});
+    setWeather([]);
+    setMap("");
+  }
+}
+
+async function getMap(newLocation) {
+  const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${newLocation.lat},${newLocation.lon}&zoom=11`;
+  setMap(API);
+}
+
+async function getWeather(newLocation) {
+  try {
+    const API = `http://localhost:8080/weather?searchQuery=${searchQuery}&lat=${newLocation.lat}&lon=${newLocation.lon}`;
+    const res = await axios.get(API);
+    console.log(res.data);
+    setWeather(res.data);
+  } catch (error) {
+    console.log(error);
+    setWeather([]);
+  }
+}
+
+  return (
+    <div className="App">
+    <h1>City Ecplorer </h1>
+    <input onChnage={handleChange} placeholder="Place Name" />
+    <button onClick={getLocation}>Explore</button>
+    <p>{apiError}</p>
+    <h2>{location.display_name}</h2>
+    {map && <img scr={map} alt="map" />}
+    {weather.map((day, index) => {
+      return (
+        <p key={index}>
+        {day.date} - {day.description}
+        </p>
+      );
+    })}
+      </div>
+  );
+}
+
+export default App;
---
+++ server/data/server.js
@@ -0,0 +1,37 @@
+import index from "./index";
+const express = require("express");
+const cors = require ("cors");
+const axios = require("axios");
+require("dotenv").config();
+const PORT = 8080;
+const app = express();
+app.use(cors());
+
+const data = require("./data/weather.json");
+
+app.get("/", (request, response) => {
+ response.status(200).json("You are looking at the root route.");
+});
+
+app.get("/weather", async (resquest, response) => {
+const { lat, lon, searchQuery } = request.query;
+
+
+const forecasts = [];
+try{
+ const API = `https://api.weatherbit.io/v2.0/forecast/daily?key=${process.env.WEATHER_API_KEY}&city=${searchQuery}`;
+ const res = await axios.get(API);
+
+ res.data.data.forEach((day) => {
+ const ff = {  date: day.valid_date, description: day.weather.description };
+ forecasts.push(fc);
+ });
+ 
+ response.status(200).json(forecasts);
+} catch (error) {
+ console.log(error);
+ response.status(404).json("Location not Found");
+}
+});
+
+app.listen(PORT, () => console.log('App is running on PORT ${PORT}'));
